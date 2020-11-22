7a1527238d1f6aee0d1ec44703a841604e5d311a
Pedro Mateus Tavares <pedromateustavares@gmail.com>
2020-11-22 20:37:55 -0300

    debugging deployment

--- lib/tune_web/endpoint.ex
+++ lib/tune_web/endpoint.ex
@@ -10,15 +10,15 @@ defmodule TuneWeb.Endpoint do
     store: :cookie,
     key: "_tune_key",
     signing_salt: "2txYZyfx",
-    encryption_salt: {__MODULE__, :config, [:session_encryption_salt]},
-    same_site: "Lax"
+    encryption_salt: {__MODULE__, :config, [:session_encryption_salt]}
   ]
 
   socket "/socket", TuneWeb.UserSocket,
     websocket: true,
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [check_origin: false, connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
