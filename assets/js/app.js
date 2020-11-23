dd74884153a1cad24881eb459c8563ef07af6f46
Pedro Mateus Tavares <pedromateustavares@gmail.com>
2020-11-22 21:00:14 -0300

    updated dependencies

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,6 @@ import $ from 'jquery';
 import ProgressBar from "progressbar.js"
 
 let Hooks = {};
-let spotifySDKReady = false;
 
 Hooks.ShareLink = {
   mounted() {
@@ -77,146 +76,6 @@ Hooks.TimeFilter = {
     })
   }
 }
-Hooks.ProgressBar = {
-  mounted() {
-    if (this.el.dataset.premium === "true") {
-      this.el.addEventListener("click", (e) => {
-        const positionMs = Math.floor(
-          (e.target.max * e.offsetX) / e.target.offsetWidth
-        );
-
-        // Optimistic update
-        this.el.value = positionMs;
-
-        this.pushEvent("seek", { position_ms: positionMs });
-      });
-    }
-  },
-};
-
-Hooks.VolumeSlider = {
-  mounted() {
-    this.el.addEventListener("change", (e) => {
-      this.pushEvent("set_volume", { volume_percent: e.target.valueAsNumber });
-    });
-  },
-};
-
-Hooks.AudioPlayer = {
-  player: null,
-  initPlayer() {
-    this.player = new Spotify.Player({
-      name: this.playerName(),
-      getOAuthToken: (cb) => {
-        cb(this.token());
-      },
-    });
-
-    this.player.addListener("ready", () => {
-      this.pushEvent("refresh_devices", {});
-      console.log("Device is ready");
-    });
-
-    this.player.addListener("initialization_error", console.error);
-    this.player.addListener("authentication_error", console.error);
-    this.player.addListener("account_error", console.error);
-    this.player.addListener("playback_error", console.error);
-
-    this.player.addListener("not_ready", ({ device_id }) => {
-      console.warn("Device ID has gone offline", device_id);
-    });
-  },
-  token() {
-    return this.el.dataset.token;
-  },
-  playerName() {
-    return this.el.dataset.playerId;
-  },
-  mounted() {
-    if (spotifySDKReady) {
-      this.initPlayer();
-      this.player.connect();
-    } else {
-      window.addEventListener("spotify.ready", () => {
-        this.initPlayer();
-        this.player.connect();
-      });
-    }
-  },
-};
-
-Hooks.GlobalShortcuts = {
-  mounted() {
-    const modal = document.querySelector("#help");
-    const modalOverlay = document.querySelector("#modal-overlay");
-    const closeButton = document.querySelector("#close-button");
-    const openButton = document.querySelector("#open-button");
-    const searchInput = document.getElementById("q");
-    const navBar = document.getElementById("navbar");
-    const deviceSelector = document.getElementById("device");
-
-    closeButton.addEventListener("click", function () {
-      modal.classList.toggle("closed");
-      modalOverlay.classList.toggle("closed");
-    });
-
-    openButton.addEventListener("click", function () {
-      modal.classList.toggle("closed");
-      modalOverlay.classList.toggle("closed");
-    });
-
-    document.addEventListener("keydown", (event) => {
-      switch (event.key) {
-        case " ":
-          event.preventDefault();
-          this.pushEvent("toggle_play_pause", {});
-          break;
-        case "a":
-          this.pushEvent("prev", {});
-          break;
-        case "d":
-          this.pushEvent("next", {});
-          break;
-        case "w":
-          this.pushEvent("inc_volume", {});
-          break;
-        case "s":
-          this.pushEvent("dec_volume", {});
-          break;
-        case "?":
-          modal.classList.toggle("closed");
-          modalOverlay.classList.toggle("closed");
-          break;
-        case "Escape":
-          modal.classList.add("closed");
-          modalOverlay.classList.add("closed");
-          break;
-        case "/":
-          event.preventDefault();
-          navBar.scrollIntoView();
-          searchInput.focus();
-          break;
-        case "q":
-          deviceSelector.focus();
-          break;
-        default:
-          break;
-      }
-    });
-  },
-};
-
-Hooks.StopKeyDownPropagation = {
-  mounted() {
-    this.el.addEventListener("keydown", (event) => {
-      if (event.key == "Escape") {
-        this.el.blur();
-      } else {
-        event.stopPropagation();
-      }
-    });
-  },
-};
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
@@ -239,11 +98,6 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)
 window.liveSocket = liveSocket;
 
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const event = new Event("spotify.ready");
-  window.dispatchEvent(event);
-  spotifySDKReady = true;
-};
 
 
 window.$ = $;
